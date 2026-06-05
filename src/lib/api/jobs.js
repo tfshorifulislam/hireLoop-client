1d6c04793cae9efd6f6e1ab5b533eaf2dd9552bc
tfshorifulislam <tfshorifulislam@gmail.com>
2026-06-05 17:24:32 +0600

    job card show success

--- src/lib/api/jobs.js
+++ src/lib/api/jobs.js
@@ -1,6 +1,16 @@
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+import { serverFetch } from "../core/server";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const getJobs = async () =>{
+    return serverFetch('/api/jobs');
+}
+
+export const getJobById = async (jobId) => {
+    return serverFetch(`/api/jobs/${jobId}`);
+}
 
 export const getCompanyJobs = async (companyId, status = 'active') => {
-    const res = await fetch(`${baseUrl}/api/jobs?companyId=${companyId}&status=${status}`)
-    return res.json()
+    const res = await fetch(`${baseUrl}/api/jobs?companyId=${companyId}&status=${status}`);
+    return res.json();
 }
